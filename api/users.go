0eb88bcc821e2ac5d5412ca58d65fac61b74c07d
muthusaravanan3186 <67668154+muthusaravanan3186@users.noreply.github.com>
2020-07-27 11:49:24 -0700

    Update users.go
    changing the correct path   "github.com/sirupsen/logrus"

--- api/users.go
+++ api/users.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"time"
 
-	log "github.com/Sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"github.com/ansible-semaphore/semaphore/db"
 
 	"github.com/ansible-semaphore/semaphore/util"
