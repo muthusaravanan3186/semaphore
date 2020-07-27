52dd15aa9f9229eb7708f1a08888634bdb8425f0
muthusaravanan3186 <67668154+muthusaravanan3186@users.noreply.github.com>
2020-07-27 11:48:41 -0700

    Update runner.go
    changing the correct path   "github.com/sirupsen/logrus"

--- api/tasks/runner.go
+++ api/tasks/runner.go
@@ -14,7 +14,7 @@ import (
 	"strings"
 	"time"
 
-	log "github.com/Sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 	"github.com/ansible-semaphore/semaphore/db"
 	"github.com/ansible-semaphore/semaphore/util"
 )
